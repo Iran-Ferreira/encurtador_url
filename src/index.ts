5318e47f59d9a5bb8f26a23e7e42aa0d613127e4
Iran Ferreira <iranferreira712@gmail.com>
2024-03-25 20:47:15 -0300

    feat: database connect and new router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from "express"
 import { URLController } from './controller/URLController';
+import { MongoConnetion } from "./database/MongoConnetion";
+/* import dotenv from 'dotenv';
+dotenv.config() */
 
 const porta = 3000
 
 const api = express()
 api.use(express.json())
 
+const database = new MongoConnetion()
+database.connect()
+
 const urlController = new URLController()
 api.post("/shorten", urlController.shorten)
+api.get("/:hash", urlController.redirect)
 
 api.listen(porta, () => {
     console.log("Servidor rodando")
