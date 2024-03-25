2ebdb33d344ebd5ed773ccc04483f88697f591a7
Iran Ferreira <iranferreira712@gmail.com>
2024-03-25 20:39:54 -0300

    feat: mongo url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,4 @@
 export const config = {
-    API_URL: "http://localhost:3000"
+    API_URL: "http://localhost:3000",
+    URL_MONGO: "mongodb+srv://iranferreira712:Naosei123@encurtadorurl.prhn4yk.mongodb.net/"
 }
